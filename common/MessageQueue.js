edd8336107b8374a48fc42ebb57a98fc0d52dae5
festo <gergely.munkacsy@gmail.com>
2017-09-13 21:36:38 +0200

    Fix the comment and the logs in MessageQueue

--- common/MessageQueue.js
+++ common/MessageQueue.js
@@ -5,8 +5,8 @@ const config = require('../common/config');
 const logger = require('../common/logger');
 
 /**
- * The aim of this file to create a common interface for the message queue. The current implementation uses Redis PubSub
- * but I hope it can be easily replace to 0QM or RabbitMQ
+ * The aim of this file to create a common interface, an adapter for the message queue.
+ * The current implementation uses Redis PubSub but I hope it can be easily replace to 0QM or RabbitMQ
  */
 
 module.exports = class MessageQueue {
@@ -30,9 +30,9 @@ module.exports = class MessageQueue {
   }
 
   on(eventType, fn) {
-    logger.silly('Registar an error handler for %s event', eventType);
+    logger.silly('Register an event handler for %s event', eventType);
     this.sub.on(eventType, (channel, message) => {
-      logger.silly('%s event received');
+      logger.silly('%s event received', eventType);
       fn(channel, message);
     });
   }
