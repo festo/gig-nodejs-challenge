bb6761184e11262ee5149fa32bd698a456813fa1
festo <gergely.munkacsy@gmail.com>
2017-09-16 15:16:44 +0200

    Add failing tests for receiver service

---
+++ test/receiver.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const assert = require('assert');
+
+describe('Receiver', () => {
+  it('Allows client to connect', (done) => {
+    done(new Error());
+  });
+
+  it('Allows multiple clients to connect', (done) => {
+    done(new Error());
+  });
+
+  it('Receives messages from client', (done) => {
+    done(new Error());
+  });
+
+  it('Sends back an ACK messages', (done) => {
+    done(new Error());
+  });
+
+  it('Saves message to the MessageQueue', (done) => {
+    done(new Error());
+  });
+});
