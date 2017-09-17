ad0afc3615edf05050dfc954d335af6fe3610564
festo <gergely.munkacsy@gmail.com>
2017-09-17 17:21:25 +0200

    Finish tests for sender service

--- test/sender.js
+++ test/sender.js
@@ -63,10 +63,96 @@ describe('Receiver', () => {
   });
 
   it('Distribute messages from message queue', (done) => {
-    done(new Error());
+
+    const pClient = new Promise((resolve) => {
+      const client = new WebSocket(config.sender.url).on('open', () => {
+        resolve(client);
+      });
+    });
+
+    const pMessageQueue = new Promise((resolve, reject) => {
+      new MessageQueue().then(resolve).catch(reject);
+    });
+
+    Promise
+      .all([pClient, pMessageQueue])
+      .then(([client, messageQueue]) => {
+        let localMessage = new Message({
+          clientId: uuid.v4(),
+          type: Message.types.TEXT,
+          message: 'Test message for MessageQueue',
+        });
+
+        client.on('message', (message) => {
+          message = Message.parse(message);
+          if (localMessage.id === message.id) {
+            done();
+          }
+        });
+
+        // To generate the bugger
+        localMessage.toProto();
+        messageQueue.publish(config.channel, localMessage.toString());
+      })
+      .catch(done);
+
   });
 
   it('Distribute messages from message queue to multiple clients', (done) => {
-    done(new Error());
+    const pClient1 = new Promise((resolve) => {
+      const client = new WebSocket(config.sender.url).on('open', () => {
+        resolve(client);
+      });
+    });
+
+    const pClient2 = new Promise((resolve) => {
+      const client = new WebSocket(config.sender.url).on('open', () => {
+        resolve(client);
+      });
+    });
+
+    const pMessageQueue = new Promise((resolve, reject) => {
+      new MessageQueue().then(resolve).catch(reject);
+    });
+
+    Promise
+      .all([pClient1, pClient2, pMessageQueue])
+      .then(([client1, client2, messageQueue]) => {
+        let localMessage = new Message({
+          clientId: uuid.v4(),
+          type: Message.types.TEXT,
+          message: 'Test message for MessageQueue',
+        });
+
+        const pClient1Message = new Promise((resolve) => {
+          client1.on('message', (message) => {
+            message = Message.parse(message);
+            resolve(message);
+          });
+        });
+
+        const pClient2Message = new Promise((resolve) => {
+          client2.on('message', (message) => {
+            message = Message.parse(message);
+            resolve(message);
+          });
+        });
+
+        Promise
+          .all([
+            pClient1Message,
+            pClient2Message,
+          ])
+          .then(([message1, message2]) => {
+            if (message1.id === localMessage.id && message2.id === localMessage.id) {
+              done();
+            }
+          });
+
+        // To generate the bugger
+        localMessage.toProto();
+        messageQueue.publish(config.channel, localMessage.toString());
+      })
+      .catch(done);
   });
 });
